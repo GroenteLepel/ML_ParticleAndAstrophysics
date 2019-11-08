5e68511e0c2de74ab859688cc545f8055ff73fdb
Daniel <d.kok@student.science.ru.nl>
2019-11-08 11:08:08 +0100

    Added decision tree, naive bayes (gaussian) and tested some configurations.

--- SuSyTruthChecker.py
+++ SuSyTruthChecker.py
@@ -1,6 +1,8 @@
 import time
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas
@@ -24,9 +26,13 @@ class SuSyTruthChecker:
         :param test_file: path to the file that contains only parameters.
         :param standardised: boolean indicating if the data needs to be
         standardised.
+        :param normalised: boolean indicating if the data needs to be
+        normalised.
         """
         self.train_file = train_file
         self.test_file = test_file
+        self.is_normalised = normalised
+        self.is_standardised = standardised
 
         self.full_train_data = pandas.read_csv(train_file, header=0)
         # Since the test data does not have any labels, we cannot check how
@@ -48,6 +54,11 @@ class SuSyTruthChecker:
             self._normalise()
 
     def _standardise(self):
+        """
+        Standardise all the data. This replaces the old data.
+        :return:
+        """
+        print("Standardising data.")
         # Define att for readability
         att = self.attributes
         standardised = \
@@ -57,7 +68,14 @@ class SuSyTruthChecker:
         self.train_data.loc[:, att] = standardised
         print("This warning has been checked and can be ignored.\n")
 
+        self.is_standardised = True
+
     def _normalise(self):
+        """
+        Normalise all the data. This replaces the old data.
+        :return:
+        """
+        print("Normalising data.")
         # Define att for readability
         att = self.attributes
         normalised = \
@@ -66,20 +84,57 @@ class SuSyTruthChecker:
         self.train_data.loc[:, att] = normalised
         print("This warning has been checked and can be ignored.\n")
 
-    def train_knn(self, n_neighbours: int = 4):
-        """
-        Trains an algorithm based on the knn-method.
-        :param n_neighbours: amount of neighbours that the method should use for
-        indicating the data.
-        :return: the accuracy of the trained set
-        """
-        start = time.time()
-        clf = KNeighborsClassifier(n_neighbors=n_neighbours)
-        clf.fit(self.train_data[self.attributes], self.train_data[self.target])
+        self.is_normalised = True
 
-        prediction = clf.predict(self.check_data[self.attributes])
+    def train(self, method: str):
+        """
+        Method for selecting a preconfigured training method applied to the
+        dataset.
+        :param method: ['knn', 'dt', 'gnb'].
+        :return:
+        """
+        if method == 'knn':
+            # 'Best' result came from normalising the data first, and resulted
+            #   in an accuracy of ~0.59, which is just a bit better than
+            #   guessing.
+            self._normalise()
+            method = 'k nearest neighbours'
+            model = KNeighborsClassifier(n_neighbors=4)
+        elif method == 'dt':
+            # Best result came from doing nothing with the data, and resulted
+            #  in an accuracy of ~0.89.
+            method = 'decision tree'
+            model = DecisionTreeClassifier(
+                min_samples_leaf=4,
+                min_samples_split=10
+            )
+        elif method == 'gnb':
+            # This method is based on the Bayesian probability that a point in
+            #  the data set is a certain class, e.g. p(x = 1), given all the
+            #  parameters for this point, y_i, so e.g. p(x = 1 | y_i). The naive
+            #  part of the method is that it considers that all these parameters
+            #  y_i are independent of each other.
+            # This method was just implemented to see the documentation from
+            #  scikit-learn, no real experimenting has been done. This delivered
+            #  an accuracy of ~0.78.
+            method = 'naive bayes (gaussian)'
+            model = GaussianNB()
+        else:
+            print("No proper training method given.")
+            return 0
+
+        print("Training data based on model", method, ".")
+        return self.run_model(model)
+
+    def run_model(self, training_model):
+        start = time.time()
+        training_model.fit(self.train_data[self.attributes],
+                           self.train_data[self.target])
+
+        prediction = training_model.predict(self.check_data[self.attributes])
         accuracy = accuracy_score(self.check_data[self.target], prediction)
 
         print("Training time: %d seconds" % (time.time() - start))
-        print(accuracy)
-        return accuracy
+        print("Accuracy of model:", accuracy)
+
+        return training_model
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from SuSyTruthChecker import SuSyTruthChecker
 
 stc = SuSyTruthChecker("data/train.csv", "data/test.csv")
 
-print(stc.train_data.size)
-stc.train_knn()
-stc._standardise()
+dt_run = stc.train('dt')
+gnb_run = stc.train('gnb')
 
-print(stc.train_data.size)
-stc.train_knn()
+# This one is last since it normalises the data, and the other training methods
+#  are influenced by this negatively.
+knn_run = stc.train('knn')
