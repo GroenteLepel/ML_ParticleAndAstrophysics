066a123b699221e59873aa667a62fc7e2aace45a
Daniel <d.kok@student.science.ru.nl>
2019-11-28 13:09:08 +0100

    Small changes.

--- run_me.py
+++ run_me.py
@@ -2,4 +2,4 @@ from SusyCrossSectionPredictor import SusyCrossSectionPredictor
 
 scsp = SusyCrossSectionPredictor('data/cross_sections_train.csv',
                                  'data/cross_sections_test.csv')
-scsp.train('seq')
+# seg_model = scsp.train('seq')
